b3ae86787dc746035a75a18fd72921e4cef65bfd
Berk Ozdogan <berk.ozdogan@firestorm.capital>
2024-08-21 17:45:04 +0300

    - update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ad_pipeline',
     packages=find_packages(include=['ad_pipeline']),
-    version='0.1.1',
+    version='0.1.2',
     description='AD Pipeline Beta',
     author='technology@firestorm.capital.com',
     install_requires=[],
